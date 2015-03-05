af62e3123af3345bd62532e5134af01557ff9930
g707175425 <707175425@qq.com>
2015-03-05 14:23:34 +0800

    增加进度条demo

--- gen/com/gys/viewpager/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.gys.viewpager;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- src/com/gys/viewpager/MainActivity.java
+++ src/com/gys/viewpager/MainActivity.java
@@ -1,13 +1,22 @@
 package com.gys.viewpager;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
 import android.widget.TextView;
+
+import com.nineoldandroids.animation.Animator;
+import com.nineoldandroids.animation.Animator.AnimatorListener;
+import com.nineoldandroids.animation.ObjectAnimator;
+import com.nineoldandroids.animation.ValueAnimator;
+import com.nineoldandroids.animation.ValueAnimator.AnimatorUpdateListener;
+import com.pascalwelsch.holocircularprogressbar.HoloCircularProgressBar;
 
 
 public class MainActivity extends Activity {
@@ -18,6 +27,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         mContainer = (MyRatioPagerContainer) findViewById(R.id.container);
+        mContainer.setBackgroundColor(Color.parseColor("#AE71E8"));
         ViewPager pager = mContainer.getViewPager();
         PagerAdapter adapter = new MyPagerAdapter();
         pager.setAdapter(adapter);
@@ -26,9 +36,12 @@ public class MainActivity extends Activity {
     private class MyPagerAdapter extends PagerAdapter {
         @Override
         public Object instantiateItem(ViewGroup container, int position) {
-            TextView view = new TextView(MainActivity.this);
-            view.setText("asdasdasdsssssssssssssssssssssssssssssssssssssssss我是第:"+position+"页");
-            view.setGravity(Gravity.CENTER);
+        	HoloCircularProgressBar view = new HoloCircularProgressBar(MainActivity.this);//(HoloCircularProgressBar) View.inflate(MainActivity.this, R.layout.view_circular_progressbar, null);
+            view.setProgressBackgroundColor(Color.parseColor("#C694EF"));
+            view.setProgressColor(Color.parseColor("#FEE756"));
+            view.setProgress(position);
+            view.setMarkerEnabled(false);
+            animate(view, null,position,2000);
             container.addView(view);
             return view;
         }
@@ -38,11 +51,51 @@ public class MainActivity extends Activity {
         }
         @Override
         public int getCount() {
-            return 5;
+            return 100;
         }
         @Override
         public boolean isViewFromObject(View view, Object object) {
             return (view == object);
         }
     }
+    
+    
+    
+    private void animate(final HoloCircularProgressBar progressBar, final AnimatorListener listener,
+            final float progress, final int duration) {
+
+    	ObjectAnimator mProgressBarAnimator = ObjectAnimator.ofFloat(progressBar, "progress", progress);
+        mProgressBarAnimator.setDuration(duration);
+
+        mProgressBarAnimator.addListener(new AnimatorListener() {
+			@Override
+			public void onAnimationCancel(Animator arg0) {
+			}
+			@Override
+			public void onAnimationEnd(Animator arg0) {
+				progressBar.setProgress(progress);				
+			}
+
+			@Override
+			public void onAnimationRepeat(Animator arg0) {
+			}
+
+			@Override
+			public void onAnimationStart(Animator arg0) {
+			}
+        });
+        if (listener != null) {
+            mProgressBarAnimator.addListener(listener);
+        }
+        mProgressBarAnimator.reverse();
+        mProgressBarAnimator.addUpdateListener(new AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(final ValueAnimator animation) {
+                progressBar.setProgress((Float) animation.getAnimatedValue());
+            }
+        });
+        progressBar.setMarkerProgress(progress);
+        mProgressBarAnimator.start();
+    }
+    
 }
